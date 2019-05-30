c11603c561bf2f24ae9d9439fdb6cf51adb084fb
Cosmo Kent <cosmolightfoot@gmail.com>
2019-05-30 09:31:41 -0700

    all tests failing due to adding async to search actions

--- src/actions/cardSearchActions.js
+++ src/actions/cardSearchActions.js
@@ -1,6 +1,9 @@
 export const NEW_SEARCH_OPTIONS = 'NEW_SEARCH_OPTIONS';
 
 export const newSearch = (searchState) => (dispatch) => {
+  dispatch()
+
+
   return fetch('https://api.magicthegathering.io/v1/cards')
     .then(body => body.json())
     .then(cards => {
