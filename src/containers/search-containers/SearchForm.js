e8b4a2242d0b08475b263bb1b736855a9c377496
Cosmo Kent <cosmolightfoot@gmail.com>
2019-08-06 17:02:41 -0700

    pre branch commit

--- src/containers/search-containers/SearchForm.js
+++ src/containers/search-containers/SearchForm.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent } fromatu 'react';
 import PropTypes from 'prop-types';
 // import ErrorBoundary from '../../utilities/ErrorBoundary';
 
