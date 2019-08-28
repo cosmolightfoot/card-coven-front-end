f7d75dba0f605c61b3005b3a6f5a6ffeea51295f
Cosmo Kent <cosmolightfoot@gmail.com>
2019-08-28 00:36:15 -0700

    added theme to header make styles function

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 import { MainHeader, MainLogo, MainNav } from '../styled-components/header-styles';
 import { Link } from 'react-router-dom';
 
+const useStyles = makeStyles(theme => ({
+  header: {
+    // color: theme.palette.secondary.dark
+    padding: theme.spacing(2)
+  },
+  cardCoven: {
+
+  }
+}));
+
 export default function Header() {
+  const classes = useStyles();
   return (
-    <AppBar color="primary" position="static">
-      <Typography variant="h2">Card Coven</Typography>
+    <AppBar className={classes.header} color="primary" position="static">
+      <Typography className={classes.cardCoven} variant="h2">Card Coven</Typography>
       <MainNav>
         <Link to="/" style={{ padding: '8px' }}>Cards</Link>
         <Link to="/about">About</Link>
